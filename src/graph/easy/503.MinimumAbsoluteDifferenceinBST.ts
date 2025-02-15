889e48684eebd55cc378a20a31f86db38e74fb7e
phunguyen <nguyendinhphu98@gmail.com>
2025-02-16 00:05:38 +0700

    feat: [503]_MinimumAbsoluteDifferenceinBST

---
+++ src/graph/easy/503.MinimumAbsoluteDifferenceinBST.ts
@@ -0,0 +1,33 @@
+/**
+Given the root of a Binary Search Tree (BST), return the minimum absolute difference between the values of any two different nodes in the tree.
+Input: root = [4,2,6,1,3]
+Output: 1
+ */
+
+import { TreeNode, convertArrayToTreeNode } from './../../utils/TreeNode.ts';
+
+function getMinimumDifference(root: TreeNode | null): number {
+	let min = Infinity;
+	const arrayNode: number[] = [];
+
+	function dfs(node: TreeNode | null) {
+		if (!node) return;
+
+		dfs(node.left);
+
+		arrayNode.push(node.val);
+
+		dfs(node.right);
+	}
+
+	dfs(root);
+
+	for (let i = 0; i < arrayNode.length - 1; i++) {
+		min = Math.min(min, arrayNode[i + 1] - arrayNode[i]);
+	}
+
+	return min;
+}
+
+const root = convertArrayToTreeNode([236, 104, 701, null, 227, null, 911]);
+console.log(getMinimumDifference(root));
--- src/utils/TreeNode.ts
+++ src/utils/TreeNode.ts
@@ -8,3 +8,28 @@ export class TreeNode {
 		this.right = right === undefined ? null : right;
 	}
 }
+
+function insertIntoBST(root: TreeNode | null, val: number): TreeNode {
+	if (root === null) {
+		return new TreeNode(val);
+	}
+	if (val < root.val) {
+		root.left = insertIntoBST(root.left, val);
+	} else {
+		root.right = insertIntoBST(root.right, val);
+	}
+	return root;
+}
+
+export function convertArrayToTreeNode(
+	input: (number | null)[]
+): TreeNode | null {
+	if (input.length === 0) {
+		return null;
+	}
+	let root: TreeNode | null = null;
+	for (const val of input) {
+		if (val !== null) root = insertIntoBST(root, val);
+	}
+	return root;
+}
